3c7dcc59a610d6960764a0769403a830f6884535
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-11-30 19:11:47 -0500

    add starter code files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import "./styles/App.css"
+import React from 'react';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
