0d474471351b6f826e3cf2737ee0178881ee513d
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-12-04 17:01:04 -0500

    working on the header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import SubHeading from '../../components/SubHeading/SubHeading';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div id="home" className="app__header app__wrapper section__padding">
+    <div className="app__wrapper_info">
+      <SubHeading />
+    </div>
+    <div className="app__wrapper_img">
+
+    </div>
   </div>
 );
 
