34278c3119fae5a995db875aa39d221d5d069acb
Yu Shi <yushi@mac-mini.mynetworksettings.com>
2022-12-05 21:11:33 -0500

    worked on the about us page

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,13 +3,22 @@ import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div id="about" className="app_aboutus app__bg flex__center section__padding">
+  <div id="about" className="app__aboutus app__bg flex__center section__padding">
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div>
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
         <button type="button" className="custom__button">Know More</button>
